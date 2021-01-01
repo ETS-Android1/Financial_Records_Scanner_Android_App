e954ad454b683145c36eda3ca017a2617c1388f6
Parsa-jalilifar <parsajalilifar@gmail.com>
2020-12-31 20:26:37 -0500

    File Share Option Completed

--- app/src/main/java/com/example/btr490project/FileAdapter.java
+++ app/src/main/java/com/example/btr490project/FileAdapter.java
@@ -1,6 +1,11 @@
 package com.example.btr490project;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -20,6 +26,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 public class FileAdapter extends RecyclerView.Adapter<FileAdapter.FileViewHolder> {
@@ -77,32 +87,76 @@ public class FileAdapter extends RecyclerView.Adapter<FileAdapter.FileViewHolder
                         switch (item.getItemId()) {
                             case R.id.file_delete:
 
-                                if (uploadCurrent.getFileUrl().equals("URL not associated")){
-                                    mDatabaseReference.child(uploadCurrent.getFileKey()).removeValue();
-                                    Toast.makeText(mContext, "File Deleted", Toast.LENGTH_SHORT).show();
+                                if (uploadCurrent.getFileUrl().equals("URL not associated")) {
+                                    mDatabaseReference.child(uploadCurrent.getFileKey())
+                                            .removeValue();
+                                    Toast.makeText(mContext, "File Deleted", Toast.LENGTH_SHORT)
+                                            .show();
                                 } else {
-                                    StorageReference fileRef = mStorage.getReferenceFromUrl(uploadCurrent.getFileUrl());
-                                    fileRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void aVoid) {
-                                            // if we removed the item from storage we remove it from database too.
-                                            mDatabaseReference.child(uploadCurrent.getFileKey()).removeValue();
-                                            Toast.makeText(mContext, "Image Deleted", Toast.LENGTH_SHORT).show();
-                                        }
-                                    }).addOnFailureListener(new OnFailureListener() {
+                                    StorageReference fileRef = mStorage
+                                            .getReferenceFromUrl(uploadCurrent.getFileUrl());
+                                    fileRef.delete()
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    // if we removed the item from storage we remove it from database too.
+                                                    mDatabaseReference
+                                                            .child(uploadCurrent.getFileKey())
+                                                            .removeValue();
+                                                    Toast.makeText(mContext, "File Deleted", Toast.LENGTH_SHORT)
+                                                            .show();
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
                                         @Override
                                         public void onFailure(@NonNull Exception e) {
-                                            Toast.makeText(mContext, "Delectation failed", Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(mContext, "Delectation failed", Toast.LENGTH_SHORT)
+                                                    .show();
                                         }
                                     });
                                 }
                                 break;
                             case R.id.file_share:
-                                Toast.makeText(mContext, "share coming soon!", Toast.LENGTH_SHORT)
-                                        .show();
+                                int SDK_INT = android.os.Build.VERSION.SDK_INT;
+                                if (SDK_INT > 8) {
+                                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                                            .permitAll().build();
+                                    StrictMode.setThreadPolicy(policy);
+
+                                    try {
+                                        if (uploadCurrent.getFileUrl()
+                                                .equals("URL not associated")) {
+                                            Toast.makeText(mContext, "File is empty", Toast.LENGTH_SHORT)
+                                                    .show();
+                                        } else {
+                                            URL url = new URL(uploadCurrent.getFileUrl());
+                                            Bitmap bitmap = BitmapFactory
+                                                    .decodeStream(url.openConnection()
+                                                                          .getInputStream());
+
+                                            File file = new File(mContext.getExternalCacheDir(), File.separator + uploadCurrent
+                                                    .getFileName() + ".jpg"); // this must change according to the type of the file we are trying to share
+                                            FileOutputStream fOut = new FileOutputStream(file);
+                                            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+                                            fOut.flush();
+                                            fOut.close();
+                                            file.setReadable(true, false);
+
+                                            final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+                                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            Uri photoURI = FileProvider
+                                                    .getUriForFile(mContext, BuildConfig.APPLICATION_ID + ".provider", file);
+                                            intent.putExtra(Intent.EXTRA_STREAM, photoURI);
+                                            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                            // we need to change this according to the type of file
+                                            intent.setType("image/*"); // set type of for all images
+                                            mContext.startActivity(Intent.createChooser(intent, "Share File via")); // title of share window
+                                        }
+                                    } catch (IOException e) {
+                                        System.out.println(e);
+                                    }
+                                }
                                 break;
                         }
-
                         return false;
                     }
                 });
