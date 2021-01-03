e402f998586e702fab97bfcc84ede1deb1aae16c
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-01-02 20:39:20 -0500

    Sprint 2 Completed

--- app/src/main/java/com/example/btr490project/ImageAdapter.java
+++ app/src/main/java/com/example/btr490project/ImageAdapter.java
@@ -106,7 +106,26 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
 
                         switch (item.getItemId()) {
                             case R.id.image_delete:
-                                Toast.makeText(mContext, "Image Delete Option Come Soon!", Toast.LENGTH_SHORT).show();
+                                StorageReference imageRef = mStorage
+                                        .getReferenceFromUrl(uploadCurrent.getImageUrl());
+                                imageRef.delete()
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                // if we removed the item from storage we remove it from database too.
+                                                mDatabaseReference
+                                                        .child(uploadCurrent.getImageKey())
+                                                        .removeValue();
+                                                Toast.makeText(mContext, "Image Deleted", Toast.LENGTH_SHORT)
+                                                        .show();
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(mContext, "Delectation failed", Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                });
                                 break;
                             case R.id.image_scan:
                                 // Create Json Body Requests
@@ -125,9 +144,40 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
                                 break;
 
                             case R.id.image_share:
+                                int SDK_INT = android.os.Build.VERSION.SDK_INT;
+                                if (SDK_INT > 8) {
+                                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                                            .permitAll().build();
+                                    StrictMode.setThreadPolicy(policy);
 
-                                Toast.makeText(mContext, "Image Share Option Come Soon!", Toast.LENGTH_SHORT).show();
+                                    try {
 
+                                        URL url = new URL(uploadCurrent.getImageUrl());
+                                        Bitmap bitmap = BitmapFactory
+                                                .decodeStream(url.openConnection()
+                                                                      .getInputStream());
+
+                                        File file = new File(mContext.getExternalCacheDir(), File.separator + uploadCurrent
+                                                .getImageName());
+                                        FileOutputStream fOut = new FileOutputStream(file);
+                                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+                                        fOut.flush();
+                                        fOut.close();
+                                        file.setReadable(true, false);
+
+                                        final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        Uri photoURI = FileProvider
+                                                .getUriForFile(mContext, BuildConfig.APPLICATION_ID + ".provider", file);
+                                        intent.putExtra(Intent.EXTRA_STREAM, photoURI);
+                                        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                        intent.setType("image/*"); // set type of the image
+                                        mContext.startActivity(Intent.createChooser(intent, "Share image via")); // title of share window
+
+                                    } catch (IOException e) {
+                                        System.out.println(e);
+                                    }
+                                }
                                 break;
                         }
 
